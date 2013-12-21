35e1ada589a5108cf1ad2db1e0190ec86ac33688
soundswarm <seantconnor@gmail.com>
2013-12-20 17:48:52 -0800

    final app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '719b0f212090238027ee4aaba00012fbebcf21b0ab66f29f5215510f4e929a2bf01faa1df7564b48a4d01e2d463589e5da97ff8209eea3f6649bbddd20512731'
